13a780e7874ceed00160cd9045b7deab738c0edf
James Gayfer <10660608+jgayfer@users.noreply.github.com>
2024-05-06 14:26:50 -0700

    Make our example more interesting
    Now we have something to look at at least.

--- examples/minimal.rs
+++ examples/minimal.rs
@@ -4,5 +4,18 @@ use bevy_light_2d::Light2dPlugin;
 fn main() {
     App::new()
         .add_plugins((DefaultPlugins, Light2dPlugin))
+        .add_systems(Startup, setup)
         .run()
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            custom_size: Some(Vec2::splat(100.)),
+            ..default()
+        },
+        ..default()
+    });
 }
