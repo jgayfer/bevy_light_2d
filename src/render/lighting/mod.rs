d4521e0c6c5f11f4d8ec5abaa0c0c851d5937c40
James Gayfer <10660608+jgayfer@users.noreply.github.com>
2024-06-14 21:02:54 -0700

    Add HDR support
    As HDR uses a different texture format, we need to set it on a per view
    basis.
    
    As the pipeline is currently more of a "one and done" construct, we need to
    make use of a specialized pipeline. It allows us to dynamically create
    the render pipeline descriptor, giving us a place to hook into if each
    view has HDR enabled.
    
    On the view node side of things, we need to ensure we're grabbing the
    specialized pipeline ID from the view entity, as we are no longer
    constructing a "static" variant of the pipeline.

--- src/render/lighting/mod.rs
+++ src/render/lighting/mod.rs
@@ -1,16 +1,30 @@
 mod node;
 mod pipeline;
+mod prepare;
 
 use bevy::{
     asset::Handle,
-    render::{render_graph::RenderLabel, render_resource::Shader},
+    ecs::component::Component,
+    render::{
+        render_graph::RenderLabel,
+        render_resource::{CachedRenderPipelineId, Shader},
+    },
 };
 
 pub use node::LightingNode;
 pub use pipeline::LightingPipeline;
+pub use prepare::prepare_lighting_pipelines;
 
 pub const LIGHTING_SHADER: Handle<Shader> =
     Handle::weak_from_u128(111120241052143214281687226997564407636);
 
 #[derive(Debug, Hash, PartialEq, Eq, Clone, RenderLabel)]
 pub struct LightingPass;
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub struct LightingPipelineKey {
+    pub hdr: bool,
+}
+
+#[derive(Component)]
+pub struct LightingPipelineId(pub CachedRenderPipelineId);
